41317d47488f87cca4ffb02b52dfb632f652be4a
Laxman <laxmanbist07@gmail.com>
2019-08-22 12:21:01 +0530

    NotMyRepo2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java -- We are not Awesome !!!!");
+        System.out.println("Hello Python -- We are not Awesome !!!!");
         System.out.println("I am not enjoying the training...");
     }
 }
